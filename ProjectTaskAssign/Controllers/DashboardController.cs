a56b8344bf75cb55f6070965442baff40a39c825
dileep-demo <dileep6441677@gmail.com>
2025-10-23 16:47:05 +0530

    Update DashboardController.cs

--- ProjectTaskAssign/Controllers/DashboardController.cs
+++ ProjectTaskAssign/Controllers/DashboardController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using ProjectTaskAssign.Data;
 
 namespace ProjectTaskAssign.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly AppDbContext context;
+
+        public DashboardController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
+            var totalProjects = context.Project.Count();
+            var totalTasks = context.Tasks.Count();
+            var totalAssignees = context.Assignees.Count();
+
+            ViewBag.TotalProjects = totalProjects;
+            ViewBag.TotalTasks = totalTasks;
+            ViewBag.TotalAssignees = totalAssignees;
+
             return View();
         }
     }
